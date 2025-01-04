5ac8e90e47975046c3c5831d75ff200b349e14c0
Matheus-Lara <matheusodelara@gmail.com>
2025-01-04 17:00:10 -0300

    feat: adds user info DTO

---
+++ pkg/integration/google_user_info_dto.go
@@ -0,0 +1,15 @@
+package integration
+
+type GoogleUserInfoDTO struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+}
+
+func NewGoogleUserInfoDTO() *GoogleUserInfoDTO {
+	return &GoogleUserInfoDTO{}
+}
