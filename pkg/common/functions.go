770090a536f8cae33c249a30a65a10f315f2ea2e
Matheus-Lara <matheusodelara@gmail.com>
2025-01-04 11:56:26 -0300

    feat: adds AES crypt to google tokens

--- pkg/common/functions.go
+++ pkg/common/functions.go
@@ -1,9 +1,14 @@
 package common
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -54,4 +59,46 @@ func GenerateHMACUsingSHA256(str, key string) string {
 func VerifyHMAC(str, key, receivedHMAC string) bool {
 	calculatedHMAC := GenerateHMACUsingSHA256(str, key)
 	return calculatedHMAC == receivedHMAC
+}
+
+func EncryptAES(plainText, key string) string {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		log.Fatalf("Error while creating new cipher: %v", err)
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		log.Fatalf("Error while creating new cipher: %v", err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], []byte(plainText))
+
+	return base64.URLEncoding.EncodeToString(cipherText)
+}
+
+func DecryptAES(cipherText, key string) string {
+	cipherTextBytes, err := base64.URLEncoding.DecodeString(cipherText)
+	if err != nil {
+		log.Fatalf("Error while decoding base64: %v", err)
+	}
+
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		log.Fatalf("Error while creating new cipher: %v", err)
+	}
+
+	if len(cipherTextBytes) < aes.BlockSize {
+		log.Fatalf("cipherText too short")
+	}
+
+	iv := cipherTextBytes[:aes.BlockSize]
+	cipherTextBytes = cipherTextBytes[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherTextBytes, cipherTextBytes)
+
+	return string(cipherTextBytes)
 }
