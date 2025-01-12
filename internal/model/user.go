f1f8a57aab29141ed1b038be89d19c413b36d9d2
Matheus-Lara <matheusodelara@gmail.com>
2025-01-12 11:10:47 -0300

    remove token da tabela de user

--- internal/model/user.go
+++ internal/model/user.go
@@ -8,15 +8,14 @@ import (
 )
 
 type User struct {
-	ID           uint      `gorm:"primary_key" json:"id"`
-	Name         string    `json:"name"`
-	Email        string    `gorm:"uniqueIndex" json:"email"`
-	Password     string    `json:"Password" validate:"min=6"`
-	Token        *string   `json:"token"`
-	UserType     *string   `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
-	RefreshToken *string   `json:"refresh_token"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID               uint       `gorm:"primary_key" json:"id"`
+	Name             string     `json:"name"`
+	Email            string     `gorm:"uniqueIndex" json:"email"`
+	Password         string     `json:"Password" validate:"min=6"`
+	UserType         *string    `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	EmailConfirmedAt *time.Time `json:"email_confirmed_at"`
+	CreatedAt        time.Time  `json:"created_at"`
+	UpdatedAt        time.Time  `json:"updated_at"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) error {
