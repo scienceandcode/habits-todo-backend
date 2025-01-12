8fcb0c53e2d7dc4c52fd885bacf6bc68f7472706
Matheus-Lara <matheusodelara@gmail.com>
2025-01-12 16:56:53 -0300

    refactor: remove unused tags

--- internal/api/dto/create_user_request_dto.go
+++ internal/api/dto/create_user_request_dto.go
@@ -1,9 +1,9 @@
 package dto
 
 type CreateUserRequestDTO struct {
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6,max=32"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func NewCreateUserRequestDTO(name, email, password string) *CreateUserRequestDTO {
