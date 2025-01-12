a9c4e592f0712e3e6302c337944a537e42b8ab32
Matheus-Lara <matheusodelara@gmail.com>
2025-01-12 11:45:21 -0300

    realiza hash de password ao salvar usuário e seta UserType

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,19 +3,19 @@ package model
 import (
 	"time"
 
-	"github.com/scienceandcode/habits-todo-backend/pkg/common"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID               uint       `gorm:"primary_key" json:"id"`
-	Name             string     `json:"name"`
-	Email            string     `gorm:"uniqueIndex" json:"email"`
-	Password         string     `json:"Password" validate:"min=6"`
-	UserType         *string    `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
-	EmailConfirmedAt *time.Time `json:"email_confirmed_at"`
-	CreatedAt        time.Time  `json:"created_at"`
-	UpdatedAt        time.Time  `json:"updated_at"`
+	ID               uint      `gorm:"primary_key" json:"id"`
+	Name             string    `json:"name"`
+	Email            string    `gorm:"uniqueIndex" json:"email"`
+	Password         string    `json:"Password" validate:"min=6"`
+	UserType         string    `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	EmailConfirmedAt time.Time `json:"email_confirmed_at"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) error {
@@ -25,7 +25,12 @@ func (user *User) BeforeCreate(tx *gorm.DB) error {
 
 func (user *User) BeforeSave(tx *gorm.DB) error {
 	user.UpdatedAt = time.Now()
-	user.Password = common.GenerateHMACUsingSHA256(user.Password, common.GetEnv("APP_ENCRYPTION_CYPHER_TEXT"))
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	user.UserType = string("USER")
 	return nil
 }
 
