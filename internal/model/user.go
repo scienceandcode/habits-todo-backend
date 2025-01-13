b6999759fb16e4dffc600e1c9bf1afffb6eee857
Matheus-Lara <matheusodelara@gmail.com>
2025-01-12 23:10:32 -0300

    feat: adapts user entity to convert response DTO and hashes password on create

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,41 +3,49 @@ package model
 import (
 	"time"
 
+	"github.com/scienceandcode/habits-todo-backend/internal/api/dto"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID               uint      `gorm:"primary_key" json:"id"`
-	Name             string    `json:"name"`
-	Email            string    `gorm:"uniqueIndex" json:"email"`
-	Password         string    `json:"Password" validate:"min=6"`
-	UserType         string    `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
-	EmailConfirmedAt time.Time `json:"email_confirmed_at"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
+	ID               uint       `gorm:"primary_key" json:"id"`
+	Name             string     `json:"name"`
+	Email            string     `gorm:"uniqueIndex" json:"email"`
+	Password         string     `json:"Password" validate:"min=8"`
+	UserType         string     `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	EmailConfirmedAt *time.Time `json:"email_confirmed_at"`
+	CreatedAt        time.Time  `json:"created_at"`
+	UpdatedAt        time.Time  `json:"updated_at"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) error {
 	user.CreatedAt = time.Now()
-	return nil
-}
+	user.EmailConfirmedAt = nil
+	user.UserType = "USER"
 
-func (user *User) BeforeSave(tx *gorm.DB) error {
-	user.UpdatedAt = time.Now()
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return err
 	}
 	user.Password = string(hashedPassword)
-	user.UserType = string("USER")
+
 	return nil
 }
 
-func NewUser(name, email, password string) *User {
+func (user *User) BeforeSave(tx *gorm.DB) error {
+	user.UpdatedAt = time.Now()
+	return nil
+}
+
+func NewUserFromCreateUserRequestDTO(dto *dto.CreateUserRequestDTO) *User {
 	return &User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:     dto.Name,
+		Email:    dto.Email,
+		Password: dto.Password,
 	}
+}
+
+func (user *User) ToUserDTO() *dto.UserDTO {
+	return dto.NewUserDTO(user.ID, user.Name, user.Email, user.UserType, user.EmailConfirmedAt)
 }
