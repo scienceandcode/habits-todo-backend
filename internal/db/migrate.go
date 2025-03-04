464068c02d4d224614d34d92832daa495b555cda
Matheus-Lara <matheusodelara@gmail.com>
2025-03-04 13:21:40 -0300

    chore: adds logger in MigrateModels routine

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -1,13 +1,16 @@
 package db
 
 import (
+	"github.com/scienceandcode/habits-todo-backend/internal/api/logger"
 	"github.com/scienceandcode/habits-todo-backend/internal/model"
 	"gorm.io/gorm"
 )
 
 func MigrateModels(db *gorm.DB) {
+	logger.Info("Migrating models")
 	db.AutoMigrate(
 		&model.GoogleOAuthToken{},
 		&model.User{},
 	)
+	logger.Info("Models migrated")
 }
