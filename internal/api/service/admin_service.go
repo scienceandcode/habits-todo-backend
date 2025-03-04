9db3a117611611c733db6196c85b0d4bd91a759b
Matheus-Lara <matheusodelara@gmail.com>
2025-03-04 10:59:10 -0300

    refactor: removes unecessary http write in service layer

--- internal/api/service/admin_service.go
+++ internal/api/service/admin_service.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/scienceandcode/habits-todo-backend/internal/db"
 )
@@ -12,8 +10,6 @@ type AdminService struct{}
 func (s *AdminService) MigrateModels(c *gin.Context) {
 	dbConn := db.GetConnection()
 	db.MigrateModels(dbConn)
-	db.SeedAdminUser(dbConn)
-	c.JSON(http.StatusOK, gin.H{"message": "Migration and seeding completed"})
 }
 
 func NewAdminService() *AdminService {
