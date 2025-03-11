5ceaecd14547f7a49590929299aa638d0d414b23
Matheus-Lara <matheusodelara@gmail.com>
2025-03-10 21:39:32 -0300

    feat: adds Migrate models i18n translate call

--- internal/api/controller/admin_controller.go
+++ internal/api/controller/admin_controller.go
@@ -6,13 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/scienceandcode/habits-todo-backend/internal/api"
 	"github.com/scienceandcode/habits-todo-backend/internal/db"
+	"github.com/scienceandcode/habits-todo-backend/internal/i18n"
 )
 
 type AdminController struct{}
 
 func (controller *AdminController) MigrateModels(c *gin.Context) {
 	db.MigrateModels(db.GetConnection())
-	api.ResponseSuccess(c, http.StatusCreated, gin.H{"message": "Model migrations completed"})
+	api.ResponseSuccess(c, http.StatusCreated, gin.H{"message": i18n.Message("Admin.MigrateModels.Response.Success")})
 }
 
 func NewAdminController() *AdminController {
